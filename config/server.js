45fa5ad85b29425834e776ad73ea73601a846d36
victorhugo_araujo <victorhugo_araujo@hotmail.com>
2020-09-29 21:10:19 -0300

    Initial strapi config

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0470c6fc221565f52d795c1131eecbd8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '050503d0-2c09-4dd6-b637-6e312e47c158'
+};
