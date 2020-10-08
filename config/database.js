aea91c4a703b6b3889d5edc4fcef4e5a3114475b
victorhugo_araujo <victorhugo_araujo@hotmail.com>
2020-10-07 21:39:53 -0300

    Config aws s3 static provider

--- config/database.js
+++ config/database.js
@@ -1,25 +1,3 @@
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'bookshelf',
-//       settings: {
-//         client: 'postgres',
-//         host: env('DATABASE_HOST', '127.0.0.1'),
-//         port: env.int('DATABASE_PORT', 5432),
-//         database: env('DATABASE_NAME', 'strapi'),
-//         username: env('DATABASE_USERNAME', 'strapi'),
-//         password: env('DATABASE_PASSWORD', 'strapi123'),
-//         ssl: env.bool('DATABASE_SSL', false),
-//       },
-//       options: {}
-//     },
-//   },
-// });
-
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL)
-
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
@@ -27,15 +5,15 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi123'),
+        ssl: env.bool('DATABASE_SSL', false),
       },
-      options: {
-        ssl: false,
-      },
+      options: {}
     },
   },
 });
+
