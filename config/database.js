6d069d3e065e68f28814aec37800ecac0062f8cd
victorhugo_araujo <victorhugo_araujo@hotmail.com>
2020-10-10 19:28:21 -0300

    Database config

--- config/database.js
+++ config/database.js
@@ -5,15 +5,15 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi123'),
+        ssl: env.bool('DATABASE_SSL', false),
       },
-      options: {
-        ssl: false,
-      },
+      options: {}
     },
   },
 });
+
